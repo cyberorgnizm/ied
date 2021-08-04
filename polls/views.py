3f54172a5cc72e9630dd74ae99f5e7dc02fbe6d0
Adole Samuel <adoleichasamuel@gmail.com>
2021-08-04 08:04:42 +0100

    feat: create detailview page for poll unit result

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import PollingUnit, AnnouncedPUResults
 
-# Create your views here.
+
+
+class PollingUniteResultView(DetailView):
+    model = PollingUnit
+    pk_url_kwarg = "uniqueid"
+    template_name = 'polls/poll_results.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["polling_unit_results"] = AnnouncedPUResults.objects.filter(polling_unit=self.get_object().uniqueid)
+        return context
