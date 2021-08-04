224748a0d2d61e60ddf32467eda9203f9ebbe956
Adole Samuel <adoleichasamuel@gmail.com>
2021-08-04 03:40:17 +0100

    feat: create models for polls app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,129 @@
 from django.db import models
 
-# Create your models here.
+
+class AnnouncedLGAResults(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    lga_name = models.CharField(max_length=50)
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'announced_lga_results'
+
+
+class AnnouncedStateResults(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    state_name = models.CharField(max_length=50)
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'announced_state_results'
+
+
+class AnnouncedWardResults(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    ward_name = models.CharField(max_length=50)
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'announced_ward_results'
+
+
+
+class States(models.Model):
+    state_id = models.AutoField(primary_key=True)
+    state_name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'states'
+
+
+class LGA(models.Model):
+    uniqueid = models.AutoField(primary_key=True)
+    lga_id = models.IntegerField()
+    lga_name = models.CharField(max_length=50)
+    state = models.IntegerField(db_column='state_id')
+    lga_description = models.TextField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'lga'
+
+
+class Party(models.Model):
+    partyid = models.CharField(max_length=11)
+    partyname = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'party'
+
+
+
+class Ward(models.Model):
+    uniqueid = models.AutoField(primary_key=True)
+    ward_id = models.IntegerField()
+    ward_name = models.CharField(max_length=50)
+    lga = models.IntegerField(db_column="lga_id")
+    ward_description = models.TextField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'ward'
+
+
+class PollingUnit(models.Model):
+    uniqueid = models.AutoField(primary_key=True)
+    polling_unit_id = models.IntegerField()
+    ward = models.IntegerField(db_column="ward_id")
+    lga = models.IntegerField(db_column="lga_id")
+    uniquewardid = models.IntegerField(null=True)
+    polling_unit_number = models.CharField(max_length=50, null=True)
+    polling_unit_name = models.CharField(max_length=50, null=True)
+    polling_unit_description = models.TextField(null=True)
+    lat = models.CharField(max_length=255, null=True)
+    long = models.CharField(max_length=255, null=True)
+    entered_by_user = models.CharField(max_length=50, null=True)
+    date_entered = models.DateTimeField(null=True)
+    user_ip_address = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        db_table = 'polling_unit'
+
+
+class AnnouncedPUResults(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    polling_unit = models.CharField(max_length=50, db_column="polling_unit_uniqueid")
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'announced_pu_results'
+
+class AgentName(models.Model):
+    name_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=13)
+    pollingunit = models.IntegerField(db_column="pollingunit_uniqueid")
+
+    class Meta:
+        db_table = 'agentname'
